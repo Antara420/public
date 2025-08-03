26b93c2ddc14bf5e85054d1c0b06307f342fe059
Anteo Augustincic <anteo.augustincic@gmail.com>
2025-08-03 14:42:52 +0200

    bobo probaj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import firebase from 'firebase/compat/app';
+import { db } from './firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { getFirestore } from 'firebase/firestore';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App(){
+const [vozila, setVozila]= useState([]);
+const [loading, setLoading]= useState(false);
+
+const ref = getFirestore().collection('Scooterino').collection('vozila');
+function getVozila(){
+  setLoading(true);
+  ref.onSnapshot((querySnapshot)=>{
+    const items = [];
+    querySnapshot.forEach((doc)=>{
+      items.push(doc.data());
+    });
+    setVozila(items);
+    setLoading(false);
+  })
+}
+  useEffect(()=>{
+    getVozila();
+  },[]);
+if(loading){return <h1>samo trenutak stranica se učitava</h1>;}
+
+  return(
+    <div>
+      <h1>vozila</h1>
+      {vozila.map((vozilo)=>(
+        <div key={vozilo.id}>
+          <h2>{vozilo.pogon}</h2>
+          <p>{vozilo.gume}</p>
+          </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCcsZmScxtiFVDC7AIG7fQPKxefgQgjs-c",
+  authDomain: "scooterino-projekt-7b6c6.firebaseapp.com",
+  databaseURL: "https://scooterino-projekt-7b6c6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "scooterino-projekt-7b6c6",
+  storageBucket: "scooterino-projekt-7b6c6.firebasestorage.app",
+  messagingSenderId: "702494137916",
+  appId: "1:702494137916:web:d497d653126ee8e556ed39",
+  measurementId: "G-SWVTZMS4QJ"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
